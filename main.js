87cab8697ed32637eb8f8a210105240efe5eab6a
Drew Nguyen <drew.ktran@gmail.com>
2019-05-23 14:02:21 -0700

    demo ready extension

--- main.js
+++ main.js
@@ -11,18 +11,8 @@ chrome.commands.onCommand.addListener(function(command) {
       chrome.tabs.update(current.id, { pinned: !current.pinned });
     });
   } else if (command == "toggle-browser-action") {
-
+    
   }
-});
-
-chrome.browserAction.onClicked.addListener(function() {
-    var w = 440;
-    var h = 220;
-    var left = (screen.width/2)-(w/2);
-    var top = (screen.height/2)-(h/2); 
-
-    chrome.windows.create({'url': 'redirect.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
-    });
 });
 
 // assigns query( assigns first arg to empty obj, second to callback)
